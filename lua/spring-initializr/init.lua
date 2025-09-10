8927cd4027fa41005c4c7350fcf52000d321ca95
josip <josipkeresman@gmail.com>
2025-09-10 22:19:30 +0200

    Add copyright header to init.lua (#12)

--- lua/spring-initializr/init.lua
+++ lua/spring-initializr/init.lua
@@ -1,4 +1,4 @@
-----------------------------------------------------------------------------
+---------------------------------------------------------------------------
 --
 -- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
 -- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
@@ -8,12 +8,23 @@
 -- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
 --
 --
+-- spring-initializr.nvim
+--
+--
+-- Copyright (C) 2025 Josip Keresman
+--
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+----------------------------------------------------------------------------
+
+----------------------------------------------------------------------------
+--
 -- Entry point for the Spring Initializr Neovim plugin.
 -- Registers all user commands and sets up the plugin.
---
---
--- License: GPL-3.0
--- Author: Josip Keresman
 --
 ----------------------------------------------------------------------------
 
